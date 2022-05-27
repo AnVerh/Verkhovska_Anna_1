05280cc380e8366d5d5e571c9c82a0f9f92a8e15
AnVerh <106372171+AnVerh@users.noreply.github.com>
2022-05-27 18:36:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace lab_1_Dict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>()
+                    {
+                        {2, 17 },
+                        {6,28 },
+                        {4,9 },
+                        {1,3 }
+                    };
+            Dictionary<int, int> dict2 = new Dictionary<int, int>();
+            foreach (KeyValuePair<int, int> el in dict)
+            {
+                int k = el.Key;
+                int v = el.Value;
+                dict2.Add(k + v, v);
+            }
+            
+            string json = JsonConvert.SerializeObject(dict2);
+            Console.WriteLine(json);
+
+        }
+    }
+}

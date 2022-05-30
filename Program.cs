5824e04ddb525fe26f7fa95aa73ded7b6360415b
AnVerh <106372171+AnVerh@users.noreply.github.com>
2022-05-30 11:25:17 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json;
+using System.Linq;
 
 
-namespace lab_1_Dict
+namespace sem_2_lab_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<int, int> dict = new Dictionary<int, int>()
-                    {
-                        {2, 17 },
-                        {6,28 },
-                        {4,9 },
-                        {1,3 }
-                    };
-            Dictionary<int, int> dict2 = new Dictionary<int, int>();
-            foreach (KeyValuePair<int, int> el in dict)
+            Console.Write("task = ");
+            string s = Console.ReadLine();
+            while (s != "")
             {
-                int k = el.Key;
-                int v = el.Value;
-                dict2.Add(k + v, v);
-            }
-            
-            string json = JsonConvert.SerializeObject(dict2);
-            Console.WriteLine(json);
+                int task = Int16.Parse(s);
+                if (task == 1)
+                {
+                    List<string> l = new List<string>();
+                    string smth;
+                    do
+                    {
+                        smth = Console.ReadLine();
+                        if (smth != "")
+                        {
+                            l.Add(smth);
+                        }
+                    } while (smth != "");
+                    Console.WriteLine("your list :");
+                    foreach (string item in l)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine("--------------");
+                    for (int i = 1; i < l.Count; i++)
+                    {
+                        string l1 = l[i - 1];
+                        string l2 = l[i];
+                        l[i] = $"{l2} {l1}";
+                    }
+                    Console.WriteLine("new list :");
+                    foreach (string word in l)
+                    {
+                        Console.WriteLine(word);
+                    }
+                }
 
+                if (task == 3)
+                {
+                    Console.WriteLine("write the string");
+                    string text = Console.ReadLine();//"rumor humor roar r road";
+                    Console.WriteLine("write the symbol");
+                    char c = Char.Parse(Console.ReadLine());
+                    string[] words = text.Split(' ');
+                    var found = from each in words
+                                where each.Last() == each.First() && each.StartsWith(c)==true && each.Count<char>()>1
+                                select each;
+                    int n = 0;
+                    foreach(string w in found)
+                    {
+                        //Console.WriteLine(w);
+                        n++;
+                    }
+                    Console.WriteLine($"There are {n} words that start and end with symbol {c}");
+                    break;
+                    Console.ReadKey();
+
+                }
+
+            }
         }
     }
 }
